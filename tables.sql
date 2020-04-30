4cc69c9e1ba3a027cca29a3381c2e13ca56ed8b5
TebohoRomeo <teboholekhalo13@gmail.com>
2020-04-30 14:41:49 +0200

    refactored some functions

---
+++ tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE visitors (
+    id SERIAL PRIMARY KEY,
+    name varchar(255),
+    age int,
+    date_of_visit date,
+    time_of_visit time,
+    assistant varchar(255),
+    comments text
+);
+
+-- Creating table.
